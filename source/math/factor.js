0a55bcae365898d5345b76af1648aaa744e9fc6d
Christopher Boaks <chris.boaks@gmail.com>
2016-07-05 23:49:21 -0400

    add factor

---
+++ source/math/factor.js
@@ -0,0 +1,50 @@
+import {primes} from './sequences';
+
+const cache = {};
+
+export class Factor {
+  static primeFactorize(n) {
+    if (n <= 0 || !Number.isInteger(n)) {
+      throw new Error('invalid input');
+    }
+    if (cache[n]) return cache[n];
+
+    const factors = {};
+    const possFactors = primes(n);
+    for (const p of possFactors) {
+      while (n % p === 0) {
+        factors[p] = (factors[p] || 0) + 1;
+        n /= p;
+      }
+    }
+    cache[n] = factors;
+    return factors;
+  }
+
+  static primeFactorList(n) {
+    const factorCounts = cache[n] || Factor.primeFactorize(n);
+    let list = [];
+    for (const val in factorCounts) {
+      const count = factorCounts[val];
+      const asList = Array(count).fill(parseInt(val));
+      list = list.concat(asList);
+    }
+    return list;
+  }
+
+  static gcd(a, b) {
+    if (a <= 0 || b <= 0 || !Number.isInteger(a) || !Number.isInteger(b)) {
+      throw new Error('invalid inputs');
+    }
+    
+    function euclid(x, y) {
+      return y === 0 ? x : euclid(y, x % y);
+    }
+
+    return euclid(a, b);
+  }
+
+  static lcm(a, b) {
+    return a * b / Factor.gcd(a, b);
+  }
+}
---
+++ test/test-factor.js
@@ -0,0 +1,103 @@
+const assert = require('chai').assert;
+
+import {Factor} from '../source/math/factor';
+
+describe('Factor', function() {
+  describe('Factor.primeFactorize', function() {
+    it('throws if given invalid input', function() {
+      assert.throws(function() {
+        Factor.primeFactorize(-7);
+      }, 'invalid input');
+      assert.throws(function() {
+        Factor.primeFactorize(0);
+      }, 'invalid input');
+      assert.throws(function() {
+        Factor.primeFactorize(1.2);
+      }, 'invalid input');
+      assert.doesNotThrow(function() {
+        Factor.primeFactorize(7);
+      });
+    });
+
+    it('returns prime factors as an object with factor counts', function() {
+      const expectedTwentyfour = {2: 3, 3: 1};
+      const expectedSixty = {2: 2, 3: 1, 5: 1};
+      assert.deepEqual(Factor.primeFactorize(24), expectedTwentyfour);
+      assert.deepEqual(Factor.primeFactorize(60), expectedSixty);
+    });
+  });
+
+  describe('Factor.primeFactorList', function() {
+    it('throws if given invalid input', function() {
+      assert.throws(function() {
+        Factor.primeFactorList(-7);
+      }, 'invalid input');
+      assert.throws(function() {
+        Factor.primeFactorList(0);
+      }, 'invalid input');
+      assert.throws(function() {
+        Factor.primeFactorList(1.2);
+      }, 'invalid input');
+      assert.doesNotThrow(function() {
+        Factor.primeFactorList(7);
+      });
+    });
+
+    it('returns prime factors as a list with possible multiples', function() {
+      const expectedTwentyfour = [2, 2, 2, 3];
+      const expectedSixty = [2, 2, 3, 5];
+      assert.deepEqual(Factor.primeFactorList(24), expectedTwentyfour);
+      assert.deepEqual(Factor.primeFactorList(60), expectedSixty);
+    });
+  });
+
+  describe('Factor.gcd', function() {
+    it('throws if given invalid inputs', function() {
+      assert.throws(function() {
+        Factor.gcd(-7, 3);
+      }, 'invalid inputs');
+      assert.throws(function() {
+        Factor.gcd(3, 0);
+      }, 'invalid inputs');
+      assert.throws(function() {
+        Factor.gcd(1.2, 3);
+      }, 'invalid inputs');
+      assert.doesNotThrow(function() {
+        Factor.gcd(7, 4);
+      });
+    });
+
+    it('returns the gcd of coprime numbers as 1', function() {
+      assert.equal(Factor.gcd(10, 21), 1);
+    });
+    
+    it('returns the gcd of non-coprime numbers', function() {
+      assert.equal(Factor.gcd(24, 60), 12);
+    });
+  });
+
+  describe('Factor.gcd', function() {
+    it('throws if given invalid inputs', function() {
+      assert.throws(function() {
+        Factor.gcd(-7, 3);
+      }, 'invalid inputs');
+      assert.throws(function() {
+        Factor.gcd(3, 0);
+      }, 'invalid inputs');
+      assert.throws(function() {
+        Factor.gcd(1.2, 3);
+      }, 'invalid inputs');
+      assert.doesNotThrow(function() {
+        Factor.gcd(7, 4);
+      });
+    });
+    
+    it('returns the lcm of coprime numbers', function() {
+      assert.equal(Factor.lcm(10, 21), 210);
+    });
+    
+    it('returns the lcm of non-coprime numbers', function() {
+      assert.equal(Factor.lcm(21, 6), 42);
+    });
+  });
+});
