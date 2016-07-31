fb2229cbbac778b7138acb7266f1e2c886f218cd
Christopher Boaks <chris.boaks@gmail.com>
2016-07-31 18:54:02 -0400

    add a hashing function

---
+++ source/string/hash.js
@@ -0,0 +1,25 @@
+const NUM_BUCKETS = 997; // a large prime number
+
+function mod(n, div) {
+  // ensures a positive value is always returned
+  return ((n % div) + div) % div;
+}
+
+function getCharCodes(str) {
+  return str.split('').map(char => char.charCodeAt(0));
+}
+
+function hashingCallback(prev, curr) {
+  const highestFiveBits = prev & 0xf8000000;
+  const lowestBitsShifted = prev << 5;
+  const shuffled = lowestBitsShifted ^ (highestFiveBits >> 27);
+  return shuffled ^ curr;
+}
+
+function hash(str) {
+  const charCodes = getCharCodes(str);
+  const baseHashVal = charCodes.reduce(hashingCallback, 0);
+  return mod(baseHashVal, NUM_BUCKETS);
+}
+
+export {hash};
