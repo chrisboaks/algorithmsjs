542697be3df148c287eccb0fd1760ff04d4dd064
Christopher Boaks <chris.boaks@gmail.com>
2016-07-31 23:00:52 -0400

    add HashTable data structure

--- source/dataStructures/hashTable.js
+++ source/dataStructures/hashTable.js
@@ -1,5 +1,46 @@
 import {LinkedList} from './linkedList';
+import {hash} from '../string/hash';
 
 class HashTable {
+  constructor(...args) {
+    this._items = [];
+    for (const a of args) {
+      this.add(a);
+    }
+  }
 
+  items() {
+    let rv = [];
+    for (const list of this._items) {
+      if (list) {
+        rv = rv.concat(list.toArray());
+      }
+    }
+    return rv;
+  }
+
+  includes(val) {
+    const hashVal = hash(val);
+    const list = this._items[hashVal];
+    return !!(list && list.includes(val));
+  }
+
+  add(val) {
+    const hashVal = hash(val);
+    if (!this._items[hashVal]) {
+      this._items[hashVal] = new LinkedList(val);
+    } else if (!this.includes(val)) {
+      this._items[hashVal].push(val);
+    }
+    return this;
+  }
+
+  delete(val) {
+    const hashVal = hash(val);
+    const valIndex = this._items[hashVal].indexOf(val);
+    this._items[hashVal] = this._items[hashVal].delete(valIndex);
+    return this;
+  }
 }
+
+export {HashTable};
---
+++ test/dataStructures/test-hashTable.js
@@ -0,0 +1,84 @@
+const assert = require('chai').assert;
+
+import {HashTable} from '../../source/dataStructures/hashTable';
+
+describe('LinkedList', function() {
+  describe('#constructor', function() {
+    it('can be constructed without values', function() {
+      const table = new HashTable();
+      assert.deepEqual(table.items(), []);
+    });
+
+    it('can be constructed with values', function() {
+      const table = new HashTable('ab', 'bc', 'cd');
+      assert.sameDeepMembers(table.items(), ['ab', 'bc', 'cd']);
+    });
+  });
+
+  describe('#items', function() {
+    it('lists all the values in the table', function() {
+      const table = new HashTable('ab', 'bc', 'cd');
+      assert.sameDeepMembers(table.items(), ['ab', 'bc', 'cd']);
+    });
+  });
+
+  describe('#includes', function() {
+    it('returns true if the item is in the table', function() {
+      const table = new HashTable('ab', 'bc', 'cd');
+      assert.isTrue(table.includes('ab'));
+      assert.isTrue(table.includes('bc'));
+      assert.isTrue(table.includes('cd'));
+    });
+
+    it('returns false if the item is not in the table', function() {
+      const table = new HashTable('ab', 'bc', 'cd');
+      assert.isFalse(table.includes('def'));
+    });
+
+    it('returns false if there are no items in the table', function() {
+      const table = new HashTable();
+      assert.isFalse(table.includes('def'));
+    });
+  });
+
+  describe('#add', function() {
+    it('adds items to the table', function() {
+      const table = new HashTable();
+      assert.isFalse(table.includes('item'));
+      table.add('item');
+      assert.isTrue(table.includes('item'));
+    });
+
+    it('does not add duplicates', function() {
+      const table = new HashTable();
+      table.add('item');
+      table.add('item');
+      assert.equal(table.items().length, 1);
+    });
+
+    it('returns the updated table', function() {
+      const table = new HashTable();
+      const rv = table.add('item');
+      assert.isTrue(rv.includes('item'));
+    });
+
+  });
+
+  describe('#delete', function() {
+    it('deletes items from the table', function() {
+      const table = new HashTable('ab', 'bc', 'cd');
+      assert.isTrue(table.includes('ab'));
+      table.delete('ab');
+      assert.isFalse(table.includes('ab'));
+    });
+
+    it('returns the updated table', function() {
+      const table = new HashTable('item');
+      const rv = table.delete('item');
+      assert.isFalse(rv.includes('item'));
+    });
+
+
+  });
+
+});
