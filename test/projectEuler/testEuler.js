aa0fbd9c1796ba3fbe98b2e30d12f516bd8039cf
Christopher Boaks <chris.boaks@gmail.com>
2016-08-21 08:50:11 -0400

    solve euler 13-15

--- test/projectEuler/testEuler.js
+++ test/projectEuler/testEuler.js
@@ -12,6 +12,9 @@ import euler009 from '../../source/projectEuler/euler009';
 import euler010 from '../../source/projectEuler/euler010';
 import euler011 from '../../source/projectEuler/euler011';
 import euler012 from '../../source/projectEuler/euler012';
+import euler013 from '../../source/projectEuler/euler013';
+import euler014 from '../../source/projectEuler/euler014';
+import euler015 from '../../source/projectEuler/euler015';
 
 describe('Project Euler', function() {
   it('solves euler001', function() {
@@ -61,4 +64,18 @@ describe('Project Euler', function() {
   it('solves euler012', function() {
     assert.equal(euler012(), 76576500);
   });
+
+  it('solves euler013', function() {
+    assert.equal(euler013(), '5537376230');
+  });
+
+  it('solves euler014', function() {
+    assert.equal(euler014(), 837799);
+  });
+
+  it('solves euler015', function() {
+    assert.equal(euler015(), 137846528820);
+  });
+
+
 });
