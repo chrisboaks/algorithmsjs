6a59367a503e12b79ae6b50c40cf68d17e6c03ae
Christopher Boaks <chris.boaks@gmail.com>
2016-09-01 21:02:04 -0400

    basic graph implementation

---
+++ source/dataStructures/graph.js
@@ -0,0 +1,106 @@
+class Edge {
+  constructor(from, to) {
+    this.from = from;
+    this.to = to;
+    // this.weight = weight;
+
+  }
+}
+
+// class Node {
+
+// }
+
+class Graph {
+  constructor(adjacency, isDirected = true) {
+    this._isDirected = isDirected;
+    this._inputAdjacency(adjacency);
+  }
+
+  _inputAdjacency(adjacency) {
+    const connectivity = adjacency
+      .split('\n')
+      .map(edgeStr => edgeStr
+        .split('')
+        .filter(x => x.match(/[01]/))
+      )
+
+    if (!connectivity.every(list => list.length === connectivity.length)) {
+      throw new Error('incomplete adjacency matrix');
+    }
+
+    this._vertices = connectivity.map(_ => []);
+    for (let fromIndex = 0; fromIndex < connectivity.length; fromIndex++) {
+      for (let toIndex = 0; toIndex < connectivity.length; toIndex++) {
+        if (connectivity[fromIndex][toIndex] === '1') {
+          this.addEdge(fromIndex, toIndex);
+        }
+      }
+    }
+  }
+
+  addEdge(from, to) {
+    if (this.exists(from, to)) {
+     return;
+    }
+
+    this._vertices[from].push(new Edge(from, to));
+
+    if (!this._isDirected) {
+      this._vertices[to].push(new Edge(to, from));
+    }
+  }
+
+  exists(from, to) {
+    return this._vertices[from].some(edge => edge.to === to);
+  }
+
+  toString() {
+    const zeros = this._vertices.map(_ => '0');
+    const summary = [];
+
+    this._vertices.forEach(vertex => {
+      const vertexSummary = zeros.slice();
+
+      vertex.forEach(edge => {
+        vertexSummary[edge.to] = '1';
+      });
+
+      summary.push(vertexSummary.join(''));
+    });
+
+    return summary.join('\n');
+  }
+
+  bfs(start = 0) {
+    const vertexStates = this._vertices.map(v => ({
+      color: 'white',
+      parent: null,
+      distance: 0
+    }));
+    const queue = [start];
+
+    while (queue.length) {
+      const currentI = queue.shift();
+      const currentV = this._vertices[currentI];
+      const currentVState = vertexStates[currentI];
+      // process vertex?
+      currentV.forEach(edge => {
+        // process edge?
+        const destinationI = edge.to;
+        const destination = vertexStates[destinationI];
+        if (destination.color === 'white') {
+          destination.color = 'gray';
+          destination.parent = currentI;
+          destination.distance = currentVState.distance + 1;
+          queue.push(destinationI);
+        }
+      });
+      currentVState.color = 'black';
+    }
+
+    return vertexStates;
+  }
+}
+
+export {Graph};
---
+++ test/dataStructures/test-graph.js
@@ -0,0 +1,76 @@
+const assert = require('chai').assert;
+
+import {Graph} from '../../source/dataStructures/graph';
+
+const gs6 =
+`010011
+101010
+010100
+001010
+110100
+100000`;
+
+const ga6 =
+`010011
+001010
+010100
+000000
+110100
+000000`;
+
+const gs6raw =
+`0 1 0 0 1 1
+1,0,1,0,1,0,
+0;1;0;1;0;0
+001010
+1-1+0#  1% 0 h 0
+   1   0  ;  00  $0  g0    `;
+
+
+describe('Graph', function() {
+  describe('#constructor', function() {
+    it('can be initialized from an arbitrarily separated table of 0s and 1s', function() {
+      const g = new Graph(gs6raw);
+      assert.equal(g.toString(), gs6);
+    });
+
+    it('throws if passed incomplete adjacency data', function() {
+      const incomplete = `11
+                          110
+                          011`;
+      assert.throws(function() {
+        const g = new Graph(incomplete);
+      }, 'incomplete adjacency matrix');
+    });
+
+    it('automatically makes symmetric adjacency data if isDirected is false', function() {
+      const g = new Graph(ga6, false);
+      assert.equal(g.toString(), gs6);
+    });
+  });
+
+  describe('#addEdge', function() {
+    it('adds directed edges', function() {
+      const input = '000\n000\n010';
+      const expected = '001\n000\n010';
+      const g = new Graph(input);
+      g.addEdge(0, 2);
+      assert.equal(g.toString(), expected);
+    });
+
+    it('adds both edges if the graph is undirected', function() {
+      const input = '000\n000\n000';
+      const expected = '001\n000\n100';
+      const g = new Graph(input, false);
+      g.addEdge(0, 2);
+      assert.equal(g.toString(), expected);
+    });
+
+    it('ignores attempts to add preexisting edges', function() {
+      const input = '001\n000\n000';
+      const g = new Graph(input);
+      g.addEdge(0, 2);
+      assert.equal(g.toString(), input);
+    });
+  });
+});
