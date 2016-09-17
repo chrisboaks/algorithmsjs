d133a5281edb752bf31a1f93ae61cc8bcb6d74d1
Christopher Boaks <christopher.boaks@newscred.com>
2016-09-17 19:20:21 -0400

    functional approach to euler001 and formally skip euler tests

--- source/projectEuler/euler001.js
+++ source/projectEuler/euler001.js
@@ -2,11 +2,9 @@
 // Find the sum of all the multiples of 3 or 5 below 1000.
 
 export default function euler001() {
-  let sum = 0;
-  for(let i = 1; i < 1000; i++) {
-    if (i % 3 === 0 || i % 5 === 0) {
-      sum += i;
-    }
-  }
-  return sum;
+  return new Array(999)
+    .fill(0)  // can't map over undefined :(
+    .map((_, i) => i + 1)
+    .filter(n => n % 3 === 0 || n % 5 === 0)
+    .reduce((prev, curr) => prev + curr);
 }
